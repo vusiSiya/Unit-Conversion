298a9ed87ad26535697662385c50b2f04016af32
Siyabonga Mahlalela <123079710+vusiSiya@users.noreply.github.com>
2023-10-23 00:53:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+const inputEl = document.querySelector("input");
+const conversionsEl = document.querySelector(".conversions");
+render();
+
+inputEl.addEventListener("change",()=>render());
+
+function render() { 
+    const number = inputEl.value;
+    const conversionText = [
+        {
+            header: "Length (Meters/Feet)",
+            metricUnit: "meters",
+            imperialUnit: "feet",
+            conversionNum: 3.2808399,
+        },
+         {
+            header: "Volume (Liters/Gallons)",
+            metricUnit: "liters",
+            imperialUnit: "gallons",
+            conversionNum: 0.26417,
+        },
+         {
+            header: "Mass (Kilograms/Pounds)",
+            metricUnit: "kilos",
+            imperialUnit: "pounds",
+            conversionNum: 2.20462
+        }
+        
+    ];
+    
+    conversionsEl.innerHTML = conversionText.reduce((acc,obj)=>{
+        let imperialConversion = number * obj.conversionNum;
+        let metricConversion = number / obj.conversionNum;
+        return acc + (
+            `<section>
+                <h5>${obj.header}</h5>
+                ${number &&
+                    `<div>
+                        <p>${number} ${obj.metricUnit} = ${imperialConversion.toFixed(3)} ${obj.imperialUnit}</p>
+                        |
+                        <p>${number} ${obj.imperialUnit} = ${metricConversion.toFixed(3)} ${obj.metricUnit}</p>
+                    </div>`
+                }
+            </section>`)
+    },"");
+    
+}
+
+
