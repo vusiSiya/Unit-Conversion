d3d96dd4ef98cca071816b50da34445e31179a8c
Siyabonga Mahlalela <123079710+vusiSiya@users.noreply.github.com>
2023-10-24 05:04:03 +0200

    making button work

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 
 const inputEl = document.querySelector("input");
+const convertBtn = document.querySelector("button");
 const conversionsEl = document.querySelector(".conversions");
 render();
 
-inputEl.addEventListener("change",()=>render());
+convertBtn.addEventListener("click", () => inputEl.value && render());
 
-function render() { 
-    const number = inputEl.value;
-    const conversionText = [
-        {
-            header: "Length (Meters/Feet)",
-            metricUnit: "meters",
-            imperialUnit: "feet",
-            conversionNum: 3.2808399,
-        },
-         {
-            header: "Volume (Liters/Gallons)",
-            metricUnit: "liters",
-            imperialUnit: "gallons",
-            conversionNum: 0.26417,
-        },
-         {
-            header: "Mass (Kilograms/Pounds)",
-            metricUnit: "kilos",
-            imperialUnit: "pounds",
-            conversionNum: 2.20462
-        }
-        
-    ];
-    
-    conversionsEl.innerHTML = conversionText.reduce((acc,obj)=>{
-        let imperialConversion = number * obj.conversionNum;
-        let metricConversion = number / obj.conversionNum;
-        return acc + (
-            `<section>
+function render() {
+  const number = inputEl.value;
+  const conversionText = [
+    {
+      header: "Length (Meters/Feet)",
+      metricUnit: "meters",
+      imperialUnit: "feet",
+      conversionNum: 3.2808399,
+    },
+    {
+      header: "Volume (Liters/Gallons)",
+      metricUnit: "liters",
+      imperialUnit: "gallons",
+      conversionNum: 0.26417,
+    },
+    {
+      header: "Mass (Kilograms/Pounds)",
+      metricUnit: "kilos",
+      imperialUnit: "pounds",
+      conversionNum: 2.20462
+    }
+  ];
+
+  conversionsEl.innerHTML = conversionText.reduce((acc, obj) => {
+    let imperialConversion = number * obj.conversionNum;
+    let metricConversion = number / obj.conversionNum;
+    return acc + (
+      `<section>
                 <h5>${obj.header}</h5>
                 ${number &&
-                    `<div>
+      `<div>
                         <p>${number} ${obj.metricUnit} = ${imperialConversion.toFixed(3)} ${obj.imperialUnit}</p>
                         |
                         <p>${number} ${obj.imperialUnit} = ${metricConversion.toFixed(3)} ${obj.metricUnit}</p>
                     </div>`
-                }
+      }
             </section>`)
-    },"");
-    
+  }, "");
+
 }
 
 
